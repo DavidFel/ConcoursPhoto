fde24e967fa996365be916588e05fa88813807ee
Ahmed MKHININI <notamail@gmail.com>
2017-05-18 10:30:46 +0200

    Gallery

--- src/main/webapp/app/gallery/gallery.controller.js
+++ src/main/webapp/app/gallery/gallery.controller.js
@@ -5,20 +5,34 @@
         .module('concoursphotoApp')
         .controller('GalleryController', GalleryController);
 
-    GalleryController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    GalleryController.$inject = ['$scope', 'Principal', 'LoginService', 'Photo', '$state'];
 
-    function GalleryController ($scope, Principal, LoginService, $state) {
+    function GalleryController ($scope, Principal, LoginService, Photo, $state) {   
         var vm = this;
-
+		vm.myFile=null;
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+        vm.MsgComment= "...";
+
+        vm.vote= function Vote(ValueVote,PhotoID,UserID) {
+			console.log ({ValueVote}) ;
+			console.log ({PhotoID}) ;
+			console.log ({UserID}) ;
+        };
+         vm.ValiderComment=function ValiderComment(text,PhotoID,UserID){
+			console.log ({text}) ;
+			console.log ({PhotoID}) ;
+			console.log ({UserID}) ;
+        };
+
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
 
         getAccount();
+        loadAll();
 
         function getAccount() {
             Principal.identity().then(function(account) {
@@ -26,6 +40,13 @@
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
+        function loadAll() {
+        	Photo.query(function(result) {
+        		vm.photos = result;
+                vm.searchQuery = null;
+        	});
+        };
+        
         function register () {
             $state.go('register');
         }
